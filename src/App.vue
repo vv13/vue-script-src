3fdbc413474d9a9dc8cf4cb93c1b93cb311bd29a
vv13 <zwhvv13@gmail.com>
2018-08-09 23:37:39 +0800

    feat: v0.1

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="https://webcert.cnmstl.net/cert/grade?sn=23e8abaade0745538be1a5971a6b9ddb" target="_blank" id="jvs-cert"></a>
+    <vue-script-src src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></vue-script-src>
+    <vue-script-src src="https://webcert.cnmstl.net/cert/grade/small_h_img.js?sn=23e8abaade0745538be1a5971a6b9ddb"></vue-script-src>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VueScriptSrc from '../dist/vueScriptSrc.umd'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    VueScriptSrc,
   }
 }
 </script>
---
+++ src/components/VueScriptSrc/VueScriptSrc.vue
@@ -0,0 +1,20 @@
+<template>
+  <div ref="wrap" class="vue_script_src" style="display: none"></div>
+</template>
+<script>
+export default {
+  name: 'vue-script-src',
+  props: {
+    src: {
+      type: String
+    }
+  },
+  mounted() {
+    const dom = document.createElement("script");
+    dom.type = "text/javascript";
+    dom.src = this.src;
+    this.$refs.wrap.appendChild(dom);
+  }
+};
+</script>
+
